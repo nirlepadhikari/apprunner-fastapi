46a11388b4fd043825d809fb65335e9756dcd01d
nirlepadhikari <71162816+nirlepadhikari@users.noreply.github.com>
2022-11-20 01:07:26 +1100

    add

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
