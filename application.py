661e9f52410de138d3681368ff802777c0753c88
nirlepadhikari <71162816+nirlepadhikari@users.noreply.github.com>
2022-11-19 23:58:41 +1100

    second

--- application.py
+++ application.py
@@ -7,7 +7,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-# configure to run in elastic beanstalk
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
